12c1adfc2e2bde69085d89801ad66975b6dd3ad5
motoki-adachi <zyx.wvu43@icloud.com>
2020-03-06 16:42:14 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       #credentails.ymlに鍵の本体があります
-      aws_access_key_id: Rails.application.credentials[aws][:access_key_id],
-      aws_secret_access_key: Rails.application.credentials[aws][:secret_access_key],
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       # aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       # aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
