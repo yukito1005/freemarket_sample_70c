56f083cf51d978bc3e5f7fcab5f52368a222ad49
motoki-adachi <zyx.wvu43@icloud.com>
2020-03-09 13:43:33 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# --ancestryによるレディースカテゴリbigin--
+lady = Category.create(:name=>"レディース")
+
+lady_tops = lady.children.create(:name=>"トップス")
+lady_ = lady.children.create(:name=>"ジャケット/アウター")
+lady_ = lady.children.create(:name=>"パンツ")
+lady_ = lady.children.create(:name=>"スカート")
+lady_ = lady.children.create(:name=>"ワンピース")
+lady_ = lady.children.create(:name=>"靴")
+lady_ = lady.children.create(:name=>"ルームウェア/パジャマ")
+lady_ = lady.children.create(:name=>"レッグウェア")
+lady_ = lady.children.create(:name=>"帽子")
+lady_ = lady.children.create(:name=>"バッグ")
+lady_ = lady.children.create(:name=>"アクセサリー")
+lady_ = lady.children.create(:name=>"ヘアアクセサリー")
+lady_ = lady.children.create(:name=>"小物")
+lady_ = lady.children.create(:name=>"時計")
+lady_ = lady.children.create(:name=>"ウィッグ/エクステ")
+lady_ = lady.children.create(:name=>"浴衣/水着")
+lady_ = lady.children.create(:name=>"スーツ/フォーマル/ドレス")
+lady_ = lady.children.create(:name=>"マタニティ")
+lady_ = lady.children.create(:name=>"その他")
+
+
