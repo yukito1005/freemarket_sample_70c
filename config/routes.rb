7aaa977df86533c63340788dce208ee002fb5722
yukito1005 <tokuoka.yukito@gmail.com>
2020-03-09 20:22:25 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root "products#new"
+  root "homes#index"
   resources :products, only: [:index,:new]
 
 
