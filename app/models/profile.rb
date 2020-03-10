6717111aefea60ea2f8ef980beb9728d242ed43b
takehiro410 <takehiro.plusultra@gmail.com>
2020-03-10 15:51:19 +0900

    update user.rb, profile.rb
    バリデーションを追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :lastname, :lastname_kana, presence: true
-  validates :firstname, :firstname_kana, presence: true
+  validates :firstname, :lastname, presence: true, format: { with: /[ぁ-んァ-ン一-龥]/, message: '全角で入力してください' }
+  validates :firstname_kana, :lastname_kana, presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/, message: '全角カタカナのみで入力してください' }
   validates :birthday, presence: true
   validates :postal_code, :prefecture, :city, :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_one :creditcard
   accepts_nested_attributes_for :profile
   # validates :email, presence: true, unique: true
-  validates :nickname, :password, :encrypted_password, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :nickname, :encrypted_password, presence: true
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: '半角で入力してください' }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
