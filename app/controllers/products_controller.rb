c3e62c0198fd1db863994966cb14331b503af7fd
motoki-adachi <zyx.wvu43@icloud.com>
2020-03-10 15:38:06 +0900

    create_product-show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,19 @@
 class ProductsController < ApplicationController
+  def index
+
+  end
+
+
   def new
   end 
 
   def show
+    @product = Product.find(params[:id])
+    @category = @product.category
+  end
+
+  def edit
+    @product = Product.find(params[:id])
   end
 
   def purchase
