4f36d33b7f8508a16bda2791f6fb86bf72d91478
takehiro410 <takehiro.plusultra@gmail.com>
2020-03-11 14:44:36 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # has_many :products, class_name: 'Product', foreign_key: 'user_id'
   has_one :profile
   has_one :creditcard
   accepts_nested_attributes_for :profile
-  # validates :email, presence: true, unique: true
+  
   validates :nickname, :encrypted_password, presence: true
   validates :password, presence: true, length: { minimum: 7 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: '半角で入力してください' }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
