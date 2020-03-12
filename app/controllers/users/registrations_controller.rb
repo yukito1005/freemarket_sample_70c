559679a910252c8167eb2037170fb0212b074bf7
takehiro410 <takehiro.plusultra@gmail.com>
2020-03-12 11:50:41 +0900

    add registrations_controller.rb
    ページを遷移しながらのデータの保存

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
 
   def new
     @user = User.new
@@ -18,10 +19,30 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render :new_profile
   end
 
+  def create_profile
+    @user = User.new(session["devise.regist_data"]["user"])
+    @profile = Profile.new(profile_params)
+    unless @profile.valid?
+      flash.now[:alert] = @profile.errors.full_messages
+      render :new_profile and return
+    end
+    @user.build_profile(@profile.attributes)
+    @user.save
+    sign_in(:user, @user)
+  end
+
   protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:firstname, :firstname_kana,
+                                    :lastname, :lastname_kana,
+                                    :postal_code, :prefecture,
+                                    :city, :address, :building,
+                                    :tell, :birthday)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
+    registrations: 'users/registrations'
   }
   devise_scope :user do
     get 'profiles', to: 'users/registrations#new_profile'
-    post 'profiles', to: 'users/registratios#create_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
   end
 
   root "homes#index"
