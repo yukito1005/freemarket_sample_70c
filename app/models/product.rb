7bef3bcd4377c3dddb005714087e0d571ce9600b
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-03-16 16:11:42 +0900

    exhibition-page sever  model fixed
    バリデーションをまとめました。

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,16 +8,10 @@ class Product < ApplicationRecord
   # belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
   belongs_to :user
   belongs_to :category
-  validates :user_id, presence: true
+  validates :user_id, :condition, :category_id, :delivery_pay, :orign_area, :lead_time, :status, presence: true
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :item_detail, presence: true, length: { maximum: 1000}
-  validates :condition, presence: true
   validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than: 10000000} 
-  validates :category_id, presence: true 
-  validates :delivery_pay, presence: true 
-  validates :orign_area, presence: true
-  validates :lead_time, presence: true
-  validates :status, presence: true
 end
 
 # minimum: 300, 
